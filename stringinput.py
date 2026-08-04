2f1fb696bdbd7cf8fc94c4d39c9c6ff6f430cb42
bhoomikagowda14 <bhoomikad886@gmail.com>
2026-08-04 10:36:56 +0530

    Create stringinput.py

---
+++ stringinput.py
@@ -0,0 +1,9 @@
+# Taking string input
+name = input("Enter your name: ")
+print("Hello,", name)
+# Taking integer input
+age = int(input("Enter your age: "))
+print("Your age is", age)
+# Taking float input
+height = float(input("Enter the height: "))
+print("Height:", height)
